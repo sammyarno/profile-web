ef37664754804927be4d3676daaa061c657ff784
Samuel Saputra <samuel.saputra@modalrakyat.id>
2021-05-16 03:46:19 +0700

    install bootstrap and fix styling folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,14 @@
+import { Button } from "react-bootstrap"
+
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World !</h1>
       </header>
+      <Button variant="primary">
+        Primary
+      </Button>
     </div>
   );
 };
