670904b8846fbeafebd8ee09313c1da947ba5871
Samuel Saputra <samuel.saputra@modalrakyat.id>
2021-05-16 02:58:39 +0700

    install node-sass and change to scss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './containers/App';
 
-import './styles/index.css';
+import './styles/index.scss';
 
 ReactDOM.render(
   <App />,
