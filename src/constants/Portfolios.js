ef13b991070d0781863fe4086661db4876de4d28
Samuel Saputra <samuel.saputra@modalrakyat.id>
2021-05-17 02:53:19 +0700

    content done

---
+++ src/constants/Portfolios.js
@@ -0,0 +1,25 @@
+const portofolios = [
+  {
+    image: 'https://creant.id/images/creant-logo.svg',
+    name: 'CRÉANT Beauty',
+    description: 'Company Profile and Online Shop for CRÉANT Beauty using ReactJs (Typescript) and ExpressJs',
+    url: 'https://creant.id',
+    themeColor: '#c7b8d9',
+  },
+  {
+    image: 'https://tomatobricks.com/images/logo_inline.png',
+    name: 'Tomato Bricks',
+    description: 'Company Profile and Project Listing for Tomato Bricks using ReactJs and ExpressJs',
+    url: 'https://tomatobricks.com',
+    themeColor: '#ae1a1f',
+  },
+  {
+    image: 'https://tutoraja.com/images/logo@2x.png',
+    name: 'Tutor Aja',
+    description: 'Official Website for Tutor Aja learning platform using ReactJs and ExpressJs',
+    url: 'https://tutoraja.com',
+    themeColor: '#f4983d',
+  },
+];
+
+export default portofolios;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,16 +1,53 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import Typed from 'react-typed';
+import portofolios from '../constants/Portfolios';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1>Hello World !</h1>
-    </header>
-    <Button variant="primary">
-      Primary
-      <FontAwesomeIcon icon={['fab', 'instagram']} />
-    </Button>
-  </div>
+  <Container className="home d-flex align-items-center justify-content-center">
+    <Row className="w-100 h-100">
+      <Col className="d-flex align-items-start justify-content-center flex-column">
+        <Typed
+          strings={["Hi, Sammy's here !"]}
+          typeSpeed={100}
+          backSpeed={100}
+          className="title"
+          loop
+        />
+        <p>
+          I am a Front End developer with industry experience building websites and web applications.
+          I specialize in JavaScript and have professional experience working with ReactJs and VueJs. Always up for a challenge!
+        </p>
+      </Col>
+      <Col className="d-flex align-items-center justify-content-center">
+        <div className="portfolio-container border rounded p-4 w-100">
+          <h4 className="mb-3">
+            <u>My Portfolios</u>
+            :
+          </h4>
+          <div className="list">
+            {
+              portofolios.map((porto, index) => (
+                <div className="item p-3" key={index}>
+                  <Row>
+                    <Col lg={3} className="d-flex align-items-center justify-content-center">
+                      <div className="image-wrapper w-100 h-100 d-flex align-items-center justify-content-center p-3">
+                        <img src={porto.image} alt="capture" className="image" />
+                      </div>
+                    </Col>
+                    <Col lg={9}>
+                      <h6 style={{ color: porto.themeColor }}>{porto.name}</h6>
+                      <p className="mb-3">{porto.description}</p>
+                      <small>{porto.url}</small>
+                    </Col>
+                  </Row>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default App;
