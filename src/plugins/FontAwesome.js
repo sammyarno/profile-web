08e8e9a0c55777fdc950ba6c10ea772078c4d096
Samuel Saputra <samuel.saputra@modalrakyat.id>
2021-05-17 01:20:43 +0700

    install fontawesome library

---
+++ src/plugins/FontAwesome.js
@@ -0,0 +1,10 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+
+library.add(
+  faInstagram,
+  faArrowRight,
+  faEye,
+);
