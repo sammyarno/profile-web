ad5355c5f069a48b4d0d88bde68228d9f7821909
Samuel Saputra <samuel.saputra@modalrakyat.id>
2021-05-30 17:09:22 +0700

    home content done

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-// import Typed from 'react-typed';
+import Typed from 'react-typed';
 import { Container } from 'react-bootstrap';
 
 import Navbar from '../components/Navbar';
@@ -6,6 +6,26 @@ import Navbar from '../components/Navbar';
 const App = () => (
   <Container className="home">
     <Navbar />
+    <div className="content">
+      <h1>
+        Hello
+        <span className="text-primary">.</span>
+      </h1>
+      <h1>
+        My name is Sam
+        <span className="text-primary">,</span>
+      </h1>
+      <h1>
+        and I am a
+        <span className="ms-3 text-primary">
+          <Typed
+            strings={['<h2>&lt;b&gt;</h2>Web Engineer<h2>&lt;/b&gt;</h2>']}
+            className="typed-wrapper"
+            typeSpeed={150}
+          />
+        </span>
+      </h1>
+    </div>
   </Container>
 );
 
