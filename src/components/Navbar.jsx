4b346f25504d9a3097e0eb3e8cfc004cc39161d0
Samuel Saputra <samuel.saputra@modalrakyat.id>
2021-05-30 16:45:33 +0700

    new navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import {
+  Row, Col,
+} from 'react-bootstrap';
+
+const Navbar = () => (
+  <Row className="navbar py-4">
+    <Col xl={3}>
+      <div className="logo-wrapper d-flex align-items-center">
+        <h3 className="me-3 fira-mono text-primary">Sam.</h3>
+      </div>
+    </Col>
+    <Col xl={6}>
+      <div className="menu-wrapper fira-mono d-flex justify-content-around">
+        <div className="menu text-center active">
+          <p>
+            &lt; Home &gt;
+          </p>
+        </div>
+        <div className="menu text-center">
+          <p>
+            &lt; About &gt;
+          </p>
+        </div>
+        <div className="menu text-center">
+          <p>
+            &lt; Projects &gt;
+          </p>
+        </div>
+        <div className="menu text-center">
+          <p>
+            &lt; Contact &gt;
+          </p>
+        </div>
+      </div>
+    </Col>
+  </Row>
+);
+
+export default Navbar;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,37 +1,12 @@
-/* eslint-disable */
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  Button, Col, Container, Row,
-} from 'react-bootstrap';
-import Slider from 'react-slick';
-import Typed from 'react-typed';
-import portofolios from '../constants/Portfolios';
-import useViewportSize from '../hooks/ViewportSize';
+// import Typed from 'react-typed';
+import { Container } from 'react-bootstrap';
 
-const App = () => {
-  const viewport = useViewportSize();
+import Navbar from '../components/Navbar';
 
-  return (
-    <Container className="home">
-      <Row className="navbar py-4">
-        <Col xl={3}>
-          <div className="logo-wrapper d-flex align-items-center">
-            <h2 className="me-3">{`<SE/>`}</h2> <p>Samuel Arno Saputra</p>
-          </div>
-        </Col>
-        <Col xl={9}>
-          <div className="menu-wrapper d-flex align-items-center justify-content-end">
-            <div className="menu active">
-              About
-            </div>
-            <div className="menu">
-              Portfolio
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+const App = () => (
+  <Container className="home">
+    <Navbar />
+  </Container>
+);
 
 export default App;
