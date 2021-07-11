e84b255f8c89599575734da35642feb72b4fabb5
Samuel Saputra <samuel.saputra@modalrakyat.id>
2021-07-11 11:48:46 +0700

    separate about content in constants

---
+++ src/constants/About.js
@@ -0,0 +1,55 @@
+export const services = [
+  {
+    title: 'Design',
+    icon: 'palette',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar quam vitae nibh',
+  },
+  {
+    title: 'Front-end Development',
+    icon: 'code',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar quam vitae nibh',
+  },
+  {
+    title: 'Back-end Development',
+    icon: 'database',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar quam vitae nibh',
+  },
+];
+
+export const skills = [
+  'React.js', 'Vue.js', 'Express.js', 'Golang', 'Typescript', 'HTML', 'CSS/SCSS',
+];
+
+export const experiences = [
+  {
+    company: 'Tokopedia',
+    title: 'Software Engineer - Web Platform',
+    duration: 'March 2021 - Now',
+    list: [
+      'Part of Home and Search Division.',
+      'Worked closely with React.js and React-Emotion.',
+      'Managed to work on a team with Agile Scrum Development process using Kanban Board.',
+    ],
+  },
+  {
+    company: 'Modal Rakyat',
+    title: 'Front-end Software Engineer',
+    duration: 'August 2019 - March 2021',
+    list: [
+      "Developed an enterprise level website for Modal Rakyat and some of its internal system to help them doing company's business. Work in a team and report directly to our CTO and Product Manager.",
+      'Worked with React.js. Vue.js, Nuxt.js, and SASS.',
+      'Managed to work on customized Agile Scrum Development process using Kanban Board.',
+    ],
+  },
+  {
+    company: 'Accelist',
+    title: 'Full-stack Software Engineer',
+    duration: 'March 2021 - Now',
+    list: [
+      'Developed an enterprise level Web and Mobile Application for a national caompany for organizing and centralizing internal data across branches and dealers. Worked in a team that comprises of a Project Manager, two Business Analysts, a Software Architect, and other Developers.',
+      'Worked with ASP.Net Core 2.2, Vue.Js, and SASS for the Web Application and Xamarin.Android for Mobile Application.',
+      'Managed to work on Agile Scrum Development process with minimum obstacle.',
+      'Received positive feedbacks from client and successfully delivered the program on time.',
+    ],
+  },
+];
--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  Container, Row, Col, Button, Tabs, Tab
+  Container, Row, Col, Tabs, Tab
 } from 'react-bootstrap';
 import useScrollInfo from 'react-element-scroll-hook';
+import { skills, experiences, services } from '../constants/About';
 
 const About = () => {
   const [scrollInfo, setRef] = useScrollInfo();
@@ -28,27 +29,17 @@ const About = () => {
           <Col xl={5}>
             <h4 className="fira-mono text-primary mb-3">Services()</h4>
             <div className="service-container">
-              <div className="service rounded p-4">
-                <div className="header d-flex justify-content-between align-items-center text-primary mb-4">
-                  <h4>Design</h4>
-                  <FontAwesomeIcon icon="palette" size="lg" />
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar quam vitae nibh</p>
-              </div>
-              <div className="service rounded p-4">
-                <div className="header d-flex justify-content-between align-items-center text-primary mb-4">
-                  <h4>Front-end Development</h4>
-                  <FontAwesomeIcon icon="code" size="lg" />
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar quam vitae nibh</p>
-              </div>
-              <div className="service rounded p-4">
-                <div className="header d-flex justify-content-between align-items-center text-primary mb-4">
-                  <h4>Back-end Development</h4>
-                  <FontAwesomeIcon icon="database" size="lg" />
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar quam vitae nibh</p>
-              </div>
+              {
+                services.map(service => (
+                  <div className="service rounded p-4">
+                    <div className="header d-flex justify-content-between align-items-center text-primary mb-4">
+                      <h4>{service.title}</h4>
+                      <FontAwesomeIcon icon={service.icon} size="lg" />
+                    </div>
+                    <p>{service.description}</p>
+                  </div>
+                ))
+              }
             </div>
           </Col>
           <Col xl={5} className="ps-xl-5">
@@ -63,13 +54,11 @@ const About = () => {
               Here are some of my <span className="text-primary">skills</span>:
             </p>
             <div className="skill-container">
-              <p className="skill text-primary">React.js</p>
-              <p className="skill text-primary">Vue.js</p>
-              <p className="skill text-primary">Express.js</p>
-              <p className="skill text-primary">Golang</p>
-              <p className="skill text-primary">Typescript</p>
-              <p className="skill text-primary">HTML</p>
-              <p className="skill text-primary">CSS/SCSS</p>
+              {
+                skills.map(skill => (
+                  <p className="skill text-primary">{skill}</p>
+                ))
+              }
             </div>
           </Col>
         </Row>
@@ -77,67 +66,28 @@ const About = () => {
           <Col xl={10}>
             <h4 className="fira-mono text-primary mb-3">Experiences()</h4>
             <div className="experience-container">
-              <Tabs className="tabs" variant="pills" defaultActiveKey="tokopedia">
-                <Tab eventKey="tokopedia" title="Tokopedia">
-                  <div className="experience">
-                    <h5 className="text-primary mb-1">Software Engineer - Web Platform</h5>
-                    <p className="mb-2">
-                      <small>March 2021 - Now</small>
-                    </p>
-                    <ul>
-                      <li>
-                        <p>Part of Home and Search Division.</p>
-                      </li>
-                      <li>
-                        <p>Worked closely with React.js and React-Emotion.</p>
-                      </li>
-                      <li>
-                        <p>Managed to work on a team with Agile Scrum Development process using Kanban Board.</p>
-                      </li>
-                    </ul>
-                  </div>
-                </Tab>
-                <Tab eventKey="modalrakyat" title="Modal Rakyat">
-                  <div className="experience">
-                    <h5 className="text-primary mb-1">Front-end Software Engineer</h5>
-                    <p className="mb-2">
-                      <small>August 2019 - March 2021</small>
-                    </p>
-                    <ul>
-                      <li>
-                        <p>Developed an enterprise level website for Modal Rakyat and some of its internal system to help them doing company's business. Work in a team and report directly to our CTO and Product Manager.</p>
-                      </li>
-                      <li>
-                        <p>Worked with React.js. Vue.js, Nuxt.js, and SASS.</p>
-                      </li>
-                      <li>
-                        <p>Managed to work on customized Agile Scrum Development process using Kanban Board.</p>
-                      </li>
-                    </ul>
-                  </div>
-                </Tab>
-                <Tab eventKey="accelist" title="Accelist">
-                  <div className="experience">
-                    <h5 className="text-primary mb-1">Full-stack Software Engineer</h5>
-                    <p className="mb-2">
-                      <small>March 2018 - Feb 2019</small>
-                    </p>
-                    <ul>
-                      <li>
-                        <p>Developed an enterprise level Web and Mobile Application for a national caompany for organizing and centralizing internal data across branches and dealers. Worked in a team that comprises of a Project Manager, two Business Analysts, a Software Architect, and other Developers.</p>
-                      </li>
-                      <li>
-                        <p>Worked with ASP.Net Core 2.2, Vue.Js, and SASS for the Web Application and Xamarin.Android for Mobile Application.</p>
-                      </li>
-                      <li>
-                        <p>Managed to work on Agile Scrum Development process with minimum obstacle.</p>
-                      </li>
-                      <li>
-                        <p>Received positive feedbacks from client and successfully delivered the program on time.</p>
-                      </li>
-                    </ul>
-                  </div>
-                </Tab>
+              <Tabs className="tabs" variant="pills">
+                {
+                  experiences.map(experience => (
+                    <Tab eventKey={experience.company} title={experience.company}>
+                      <div className="experience">
+                        <h5 className="text-primary mb-1">{experience.title}</h5>
+                        <p className="mb-2">
+                          <small>{experience.duration}</small>
+                        </p>
+                        <ul>
+                          {
+                            experience.list.map(item => (
+                              <li>
+                                <p>{item}</p>
+                              </li>
+                            ))
+                          }
+                        </ul>
+                      </div>
+                    </Tab>
+                  ))
+                }
               </Tabs>
             </div>
           </Col>
