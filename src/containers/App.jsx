89dde7a4d8fd527b7ab09f8e590d9fcb9895d8fd
Samuel Saputra <samuel.saputra@modalrakyat.id>
2021-08-09 01:22:13 +0700

    change favicon

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,30 +1,13 @@
-import useViewportSize from '../hooks/ViewportSize';
-
 import Routes from '../components/routes/index';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-import UnderConstruction from './UnderConstruction';
-
-const App = () => {
-  const viewportSize = useViewportSize();
-
-  return (
-    <>
-      {
-        process.env.NODE_ENV === 'development' || viewportSize.isDesktop
-          ? (
-            <>
-              <Navbar />
-              <Routes />
-              <Footer />
-            </>
-          ) : (
-            <UnderConstruction />
-          )
-      }
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Navbar />
+    <Routes />
+    <Footer />
+  </>
+);
 
 export default App;
