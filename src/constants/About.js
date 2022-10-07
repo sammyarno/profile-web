9c0280cf05f9fec870367dd12d7ae40081e4c4f3
Samuel Saputra <samuel.saputra@modalrakyat.id>
2022-10-07 18:59:32 +0700

    add beyondsoft to experiences

--- src/constants/About.js
+++ src/constants/About.js
@@ -22,15 +22,28 @@ export const skills = [
 
 export const experiences = [
   {
+    company: 'Beyondsoft',
+    logo: '/images/beyondsoft.png',
+    url: 'https://beyondsoft.com/',
+    title: 'ReactJs Developer',
+    duration: 'May 2022 - Now',
+    list: [
+      'Worked for DBS Bank Singapore',
+      'Part of Transformers Team',
+      'Worked closely with Next.js and SASS and OpenText TeamSite CMS',
+      'Managed to work on a team with Agile Scrum Development process using Kanban Board',
+    ],
+  },
+  {
     company: 'Tokopedia',
     logo: '/images/tokopedia.png',
     url: 'https://tokopedia.com',
     title: 'Software Engineer - Web Platform',
-    duration: 'March 2021 - Now',
+    duration: 'March 2021 - May 2022',
     list: [
-      'Part of Home and Search Division.',
-      'Worked closely with React.js and React-Emotion.',
-      'Managed to work on a team with Agile Scrum Development process using Kanban Board.',
+      'Part of Home and Search Division',
+      'Worked closely with React.js and React-Emotion',
+      'Managed to work on a team with Agile Scrum Development process using Kanban Board',
     ],
   },
   {
@@ -40,22 +53,10 @@ export const experiences = [
     title: 'Front-end Software Engineer',
     duration: 'August 2019 - March 2021',
     list: [
-      "Developed an enterprise level website for Modal Rakyat and some of its internal system to help them doing company's business. Work in a team and report directly to our CTO and Product Manager.",
-      'Worked with React.js. Vue.js, Nuxt.js, and SASS.',
-      'Managed to work on customized Agile Scrum Development process using Kanban Board.',
-    ],
-  },
-  {
-    company: 'Accelist',
-    logo: '/images/accelist.png',
-    url: 'https://accelist.com',
-    title: 'Full-stack Software Engineer',
-    duration: 'March 2018 - Feb 2019',
-    list: [
-      'Developed an enterprise level Web and Mobile Application for a national caompany for organizing and centralizing internal data across branches and dealers. Worked in a team that comprises of a Project Manager, two Business Analysts, a Software Architect, and other Developers.',
-      'Worked with ASP.Net Core 2.2, Vue.Js, and SASS for the Web Application and Xamarin.Android for Mobile Application.',
-      'Managed to work on Agile Scrum Development process with minimum obstacle.',
-      'Received positive feedbacks from client and successfully delivered the program on time.',
+      "Developed an enterprise level website for Modal Rakyat and some of its internal system to help them doing company's business",
+      'Worked in a team and report directly to our CTO and Product Manager',
+      'Worked with React.js. Vue.js, Nuxt.js, and SASS',
+      'Managed to work on customized Agile Scrum Development process using Kanban Board',
     ],
   },
 ];
