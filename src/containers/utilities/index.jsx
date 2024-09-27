206b89a8a410cac822095a2a1e369663af898e05
Samuel <samuelarnosaputra@gmail.com>
2024-09-27 14:12:24 +0700

    fix stylng on uutilities page

--- src/containers/utilities/index.jsx
+++ src/containers/utilities/index.jsx
@@ -6,8 +6,8 @@ const Utilities = () => (
     <Row className="content d-flex align-items-start justify-content-center">
       <Col xs={12} xl={10}>
         <div className="utilitiy-wrapper d-flex flex-md-row flex-column gap-4">
-          <Link className="utility border border-primary p-3 text-capitalize" to="/utilities/split-bill">
-            <h4 className="font-weight-bold text-center">split bill</h4>
+          <Link className="utility border border-primary px-3 py-2 text-capitalize" to="/utilities/split-bill">
+            <p className="font-weight-bold text-center"><b>split bill</b></p>
           </Link>
         </div>
       </Col>
