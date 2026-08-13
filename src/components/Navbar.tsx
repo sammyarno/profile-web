1a8e3a54d741156a6e16520c16be6efbe91b57c2
sammyarno <samuelarnosaputra@gmail.com>
2026-08-13 13:06:23 +0700

    perf: inline hamburger/close SVGs in Navbar instead of react-icons import
    Also drops a useMemo that memoized a freshly-created function/string on
    every render, which did nothing useful.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useMemo, useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { useState } from 'react';
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -16,14 +15,11 @@ const Navbar = () => {
 
   const toggleMenu = () => setShowMenu(show => !show);
 
-  const menuClass = useMemo(
-    () => (url?: string) => cx('text-center', pathname === url && 'bg-primary/40'),
-    [pathname]
-  );
+  const menuClass = (url?: string) => cx('text-center', pathname === url && 'bg-primary/40');
 
-  const mobileMenuClass = useMemo(
-    () => cx('absolute top-[66px] left-0 w-full bg-secondary block z-10 md:hidden', showMenu ? 'show' : 'hidden'),
-    [showMenu]
+  const mobileMenuClass = cx(
+    'absolute top-[66px] left-0 w-full bg-secondary block z-10 md:hidden',
+    showMenu ? 'show' : 'hidden'
   );
 
   return (
@@ -55,7 +51,15 @@ const Navbar = () => {
           <div className="flex justify-end md:hidden">
             <div className="px-3">
               <div className="text-end" onClick={toggleMenu} role="presentation">
-                {showMenu ? <FaTimes className="text-lg" /> : <FaBars className="text-lg" />}
+                {showMenu ? (
+                  <svg viewBox="0 0 352 512" className="inline-block h-[1em] w-[0.78em] fill-current align-[-0.125em]">
+                    <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
+                  </svg>
+                ) : (
+                  <svg viewBox="0 0 448 512" className="inline-block h-[1em] w-[1em] fill-current align-[-0.125em]">
+                    <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
+                  </svg>
+                )}
               </div>
             </div>
           </div>
